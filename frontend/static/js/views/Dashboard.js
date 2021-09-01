559f92cf4eaf30fe1ef096e0a195afa72c38786d
Jerimiah W. Hinnen <70825145+jhinnen1@users.noreply.github.com>
2021-09-01 13:28:40 -0500

    Update Dashboard.js

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -12,8 +12,11 @@ export default class extends AbstractView {
         <p>
             You have reached the dashboard. This application utilizes JavaScript/Node.js/Express.js and 
             demonstrates the "single page application" concept. Notice that you can navigate the menu, link, 
-            and browser arrows without the page refreshing.
+            and browser arrows without the page refreshing. Instead of the webpage changing, the HTML changes.
+            <br></br>
+            -Jerimiah
         </p>
+            <br></br>
         <p>
             <a href="/posts" data-link>View recent posts</a>.
         </p>
